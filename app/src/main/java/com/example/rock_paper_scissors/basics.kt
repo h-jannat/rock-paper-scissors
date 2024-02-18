9a8fbb42f936871e0bf1f59c2faf658a23eacae0
h-jannat <h.jannat@lamah.com>
2024-02-18 15:50:43 +0200

    first commit

---
+++ app/src/main/java/com/example/rock_paper_scissors/basics.kt
@@ -0,0 +1,10 @@
+package com.example.rock_paper_scissors
+
+fun main(){
+    val constantNumber=7
+    var mutableString="kkkk"
+    println(constantNumber)
+    println(mutableString)
+    mutableString="aaa"
+    println(mutableString)
+}
